0853c309fe34f55dde07a6d8f8e5a84c90cce765
Sky <anvandp18@gmail.com>
2018-10-03 15:21:58 -0700

    now the question in admin have multi answer. check it out from admin site

--- polls_app/admin.py
+++ polls_app/admin.py
@@ -1,5 +1,25 @@
+# another way to import things
+# from . import models
+# admin.site.register(models.Question)
+# admin.site.register(models.Choice)
+
 from django.contrib import admin
 from .models import Choice, Question
 
-admin.site.register(Question)
+# Change the header of the admin site
+admin.AdminSite.site_header = 'My Awesome Polls Panel!'
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldset = [
+		('Question Information', {'fields': ['question_text']}),
+		('Date and Time', {'fields': ['pub_date']}), 
+	]
+
+	inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
